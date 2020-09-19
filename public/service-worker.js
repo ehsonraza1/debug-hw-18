c66c6d354087140e7e9f577b9bd9b3b9bc84ce92
Ehson Raza <ehson.raza1@outlook.com>
2020-09-19 14:32:01 -0500

    All the changes made to debug service-worker and webpack

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,10 @@
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
+  "/index.js",
   "/styles.css",
-  "/dist/app.bundle.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
 ];
 
 const PRECACHE = "precache-v1";
